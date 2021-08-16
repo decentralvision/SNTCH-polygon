2a62bac86efd8c8c2db55fe6929295345fbb4bc4
Tom K <36858744+decentralvision@users.noreply.github.com>
2021-08-16 01:04:24 -0700

    chang

--- layer0.config.js
+++ layer0.config.js
@@ -1,3 +1,3 @@
 // This file was automatically added by layer0 deploy.
 // You should commit this file to source control.
-module.exports = {"backends":{"origin":"localhost:5000"}}
+module.exports = {"backends":{"origin":{"url":'https://closed-magic-polygon-master-3.free.layer0-perma.link"'}}}
