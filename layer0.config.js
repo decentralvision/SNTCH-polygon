653af8a43ed3c2f239f140186e3d57e5f0a2f239
Tom K <36858744+decentralvision@users.noreply.github.com>
2021-08-16 01:26:16 -0700

    chang

--- layer0.config.js
+++ layer0.config.js
@@ -1,9 +1,9 @@
 // This file was automatically added by layer0 deploy.
 // You should commit this file to source control.
 module.exports = {
+    connector: '@layer0/react',
     backends: {
       origin: {
-        connector: '@layer0/react'
         domainOrIp: 'closed-magic-polygon-master-3.free.layer0-perma.link',
         hostHeader: 'closed-magic-polygon-master-3.free.layer0-perma.link',
       },
